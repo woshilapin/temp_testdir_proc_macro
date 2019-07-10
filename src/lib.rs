755082ebb0dfdf5608bff2a793c7c40e98309c7c
Jean SIMARD <woshilapin@tuziwo.info>
2019-07-10 11:26:23 +0200

    Create an iterator for AttributeArgs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,56 @@
+extern crate alloc;
 extern crate proc_macro;
 
+use alloc::vec::IntoIter;
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2, TokenTree};
 use quote::quote;
 use syn::{parse_macro_input, AttributeArgs, Lit, Meta, NestedMeta};
+
+#[derive(Debug, PartialEq)]
+enum TempDirMacroArgument {
+    Ignore,
+    Path(String),
+}
+
+struct TempDirMacroArgumentIterator {
+    iter_nested_meta: IntoIter<NestedMeta>,
+}
+
+impl From<IntoIter<NestedMeta>> for TempDirMacroArgumentIterator {
+    fn from(iter_nested_meta: IntoIter<NestedMeta>) -> Self {
+        TempDirMacroArgumentIterator { iter_nested_meta }
+    }
+}
+
+impl Iterator for TempDirMacroArgumentIterator {
+    type Item = TempDirMacroArgument;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter_nested_meta
+            .next()
+            .and_then(|attribute| match attribute {
+                NestedMeta::Meta(Meta::NameValue(name_value)) => {
+                    if name_value.ident == "path" {
+                        match name_value.lit {
+                            Lit::Str(value) => Some(TempDirMacroArgument::Path(value.value())),
+                            _ => None,
+                        }
+                    } else {
+                        None
+                    }
+                }
+                NestedMeta::Meta(Meta::Word(ident)) => {
+                    if ident == "ignore" {
+                        Some(TempDirMacroArgument::Ignore)
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            })
+    }
+}
 
 // TODO: Add documentation
 #[proc_macro_attribute]
@@ -73,4 +120,26 @@ pub fn test_with_tempdir(attributes: TokenStream, input: TokenStream) -> TokenSt
         }
     }
     input.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod tempdir_macro_argument_iterator {
+        use super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn from() {
+            let ignore =
+                NestedMeta::from(Meta::Word(Ident::new("ignore", Span::call_site())));
+            let not_a_valid_arg =
+                NestedMeta::from(Meta::Word(Ident::new("not_valid_arg", Span::call_site())));
+            let attribute_args = vec![ignore, not_a_valid_arg];
+            let mut iter = TempDirMacroArgumentIterator::from(attribute_args.into_iter());
+            assert_eq!(iter.next().unwrap(), TempDirMacroArgument::Ignore);
+            assert_eq!(iter.next(), None);
+        }
+    }
 }
